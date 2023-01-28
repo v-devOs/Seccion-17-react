687ecccafac76e3eac8259d78e46f597f1d59b84
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-01-28 11:47:38 -0600

    Configuracion de journalSlice

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const journalSlice = createSlice({
+  name: 'journal',
+  initialState: {
+    isSaving: true,
+    messageSave: '',
+    notes: [],
+    active: null,
+  },
+  reducers: {
+    addNewEmptyNote: ( state, action ) => {
+      
+    },
+    setActiveNote: ( state, action ) => {
+      
+    },
+    setNotes: ( state, action ) => {
+    
+    },
+    setSaving: ( state ) => {
+    
+    },
+    updateNote: ( state, action ) => {
+    
+    },
+    deleteNoteById: (state, action ) => {
+    
+    }
+    
+  },
+})
+
+export const { 
+  addNewEmptyNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+  deleteNoteById 
+} = journalSlice.actions
