2316c1736a6d438bb83008c1a5d939f7e6fb3b9d
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-01-28 12:51:01 -0600

    creando coleccion en la base de datos

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,17 +3,29 @@ import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useDispatch, useSelector } from 'react-redux'
+import { startNewNote } from '../../store/journal/thunks'
 
 export const JournalPage = () => {
+
+  const { isSaving, active } = useSelector( state => state.journal)
+  const dispatch = useDispatch()
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote())
+  }
+
   return (
     <JournalLayout>
-      {/* <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, illo quam! Quasi commodi dicta rerum sed earum maxime laborum id perspiciatis a debitis alias officia ad cumque, vitae temporibus rem illo omnis tempore. Corrupti provident fugit et natus accusamus libero dolor? Hic, odit, inventore ratione vero saepe, placeat nobis velit distinctio cumque consectetur quisquam explicabo provident cupiditate porro ipsa assumenda libero eius molestiae voluptatibus! Delectus impedit repellat tempore nemo unde exercitationem, ex minus molestiae praesentium sapiente. Repellat numquam facere doloremque quaerat atque ad asperiores saepe. Voluptates quod ullam dolor, qui reprehenderit rem rerum sapiente excepturi labore eos cum! Officia, dignissimos.
-      </Typography> */}
-      <NothingSelectedView/>
-      {/* <NoteView /> */}
+      
+      {
+        active ? <NoteView /> : <NothingSelectedView/>
+      }
 
 
       <IconButton
+        disabled={isSaving}
+        onClick={onClickNewNote}
         size='large'
         sx={{
           color: 'white',
